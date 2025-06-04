3ebb17d63a341c3efaefc055a683ebde3355e1b1
omarsito67 <125461520+omarsito67@users.noreply.github.com>
2025-06-04 16:15:17 +0200

    four

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+let nbBois = 0;
+let nbPierre = 0;
+let nbCarburant = 0;
+let nbRessourceFour = 0;
 
+
+function ajouterBois() {
+    nbBois++;
+    document.getElementById("bois").textContent = nbBois;
+}
+
+function ajouterPierre() {
+    nbPierre++;
+    document.getElementById("pierre").textContent = nbPierre;
+}
+
+function modifierCarburant(val) {
+    nbCarburant += val;
+    if (nbCarburant < 0) nbCarburant = 0;
+    document.getElementById("carburant").textContent = nbCarburant;
+}
+
+function modifierRessourceFour(val) {
+    nbRessourceFour += val;
+    if (nbRessourceFour < 0) nbRessourceFour = 0;
+    document.getElementById("ressourceFour").textContent = nbRessourceFour;
+}
