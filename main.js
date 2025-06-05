2d73a4e5f42c99b20586df2730c2d4f21484ae48
kukoji <thomasbagot5@gmail.com>
2025-06-05 10:19:08 +0200

    Creation base front end

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
+let linkMettreAJourAffichageButton =  document.getElementById("mettreAJourAffichageButton");
+let linkMinerCharbonButton =  document.getElementById("minerCharbonButton");
+let linkAffichageCharbonSpan =  document.getElementById("affichageCharbonSpan");
 
+var charbon = 0;
+
+function main() {
+    linkMettreAJourAffichageButton.addEventListener("click", () => {
+        miseAJourAffichage();
+    })
+    linkMinerCharbonButton.addEventListener("click", () => {
+        minerCharbon();
+    })
+}
+
+function miseAJourAffichage() {
+    if (charbon > 0) {
+        linkAffichageCharbonSpan.style = "";
+        linkAffichageCharbonSpan.innerHTML = "Charbon x" + charbon;
+    } else {
+        linkAffichageCharbonSpan.style = "display:none;";
+    }
+}
+
+function minerCharbon() {
+    charbon++;
+}
+
+main();
